0f7aa7cbc64270c70b3075bda84741a52567db22
foreverglorydev <happyyada.glory@gmail.com>
2022-04-27 04:18:51 -0400

    image___ social

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,9 +5,28 @@ import WalletConnect from "../../components/wallet/walletconnect";
 
 const Logo = 'images/logo.png';
 
+const etherscan = 'images/social-section/etherscan-logo.png';
+const discord = 'images/social-section/discord-logo.png';
+const telegram = 'images/social-section/telegram-logo.png';
+const twitter = 'images/social-section/twitter-logo.png';
+
+function clickSocial() 
+{
+    alert();
+}
+
 function ComponentFooter() {
     return (
-        <></>
+        <>
+            <div className='masthead'>
+                <div className='social_area'>
+                    <a><img src = {etherscan} onClick = {clickSocial} /></a>
+                    <a><img src = {discord} onClick = {clickSocial}  /></a>
+                    <a><img src = {telegram} onClick = {clickSocial}  /></a>
+                    <a><img src = {twitter} onClick = {clickSocial}  /></a>
+                </div>
+            </div>
+        </>
     )
 }
 

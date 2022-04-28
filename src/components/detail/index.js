f92c4b89fb27e9382e9bcbff2c410840a4b65429
foreverglorydev <happyyada.glory@gmail.com>
2022-04-28 13:20:08 -0400

    btn animation

--- src/components/detail/index.js
+++ src/components/detail/index.js
@@ -1,9 +1,59 @@
-import React from 'react';
+
+import React, { useContext, useEffect, useState } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useWeb3React } from '@web3-react/core'
+import Web3 from 'web3';
+import { ethers } from 'ethers';
+
+import { CONTRACTS, CONTRACTS_TYPE } from '../../utils/constants';
+
+
+let web3, contractObject1, provider, signer;
 
 const pumpkin = 'images/pumpkin.png';
 
 function ComponentDetail() {
+
+    const {
+        active,
+        account,
+        library,
+        chainId,
+        connector,
+        activate,
+        deactivate,
+      } = useWeb3React()
+
+    const [loading, setLoading] = useState(false)
+    const [stakeValue, setStakeValue] = useState()
+
+    const { ethereum } = window;
+
+    let metadata = CONTRACTS[CONTRACTS_TYPE.BAKED_BEANS][chainId]?.abi;
+    let addr = CONTRACTS[CONTRACTS_TYPE.BAKED_BEANS][chainId]?.address;
+
+    let balance = 0;
+
+    const onChangeStakeAmount = async (e) => 
+    {
+        setStakeValue(e.target.value);
+    }
+
+    useEffect(() => {
+		(async () => {
+			if (account && chainId && library) {
+                web3 = new Web3(library.provider)
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                contractObject1 = new ethers.Contract(addr, metadata, signer);
+                console.log(contractObject1);
+                balance = await contractObject1.getBalance(account);
+                console.log(balance);
+                console.log('rewardsrewards');
+			}
+		})();
+	},[chainId, account]);
+
     return (
         <div className="masthead">
             <Container>
@@ -14,7 +64,7 @@ function ComponentDetail() {
                         <div className='stakeDetail'>
                             <div className='stakeDetailText'>
                                 <p>Farm: 1,000,000 BNB</p>
-                                <p>Your Seeds: 0.00 BNB</p>
+                                <p>Your Seeds:{balance} BNB</p>
                                 <p>Daily-Growing: 6%</p>
                                 <p>Farmer: 2%</p>
                                 <div>
--- src/components/stake/index.js
+++ src/components/stake/index.js
@@ -6,7 +6,7 @@ import { ethers } from 'ethers';
 
 import { CONTRACTS, CONTRACTS_TYPE } from '../../utils/constants';
 
-let web3, contractObject1;
+let web3, contractObject1, provider, signer;
 
 function ComponentStake() {
     const {
@@ -22,31 +22,40 @@ function ComponentStake() {
     const [loading, setLoading] = useState(false)
     const [stakeValue, setStakeValue] = useState()
 
+    const { ethereum } = window;
+
     let metadata = CONTRACTS[CONTRACTS_TYPE.BAKED_BEANS][chainId]?.abi;
     let addr = CONTRACTS[CONTRACTS_TYPE.BAKED_BEANS][chainId]?.address;
+
+    let rewards;
 
     const onChangeStakeAmount = async (e) => 
     {
         setStakeValue(e.target.value);
     }
 
+    useEffect(() => {
+		(async () => {
+			if (account && chainId && library) {
+                web3 = new Web3(library.provider)
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                contractObject1 = new ethers.Contract(addr, metadata, signer);
+                console.log(contractObject1);
+                rewards = await contractObject1.beanRewards(account);
+                console.log(rewards);
+                console.log('rewardsrewards');
+			}
+		})();
+	},[chainId, account]);
+
     const onClickStake = async () => 
     {
 
         if (account && chainId && library) {
-            
-
-
             setLoading(true)
-            const { ethereum } = window;
-      
-            web3 = new Web3(library.provider)
-            const provider = new ethers.providers.Web3Provider(ethereum);
-            const signer = provider.getSigner();
-            contractObject1 = new ethers.Contract(addr, metadata, signer);
             let balance  = await contractObject1.hatchEggs(account);
             console.log(balance);
-
         }
     }
 
@@ -59,7 +68,7 @@ function ComponentStake() {
                         <div className='stakeWood'>
                             <input type="text" onChange = {onChangeStakeAmount} style={{ fontSize:'20px', textAlign:'center', width:'70%', margin:'14% 15% 0 15%', border:'1px solid black'}} />
                             <button className="mint-number-button" onClick={onClickStake} >Sowing</button>
-                            <p style={{margin:'7% 29%', fontSize:'35px'}}>Rewards: 0.00 BNB</p>
+                            <p style={{margin:'7% 29%', fontSize:'35px'}}>Rewards: {rewards} BNB</p>
                             <div className='stakeButtonGroup'>
                                 <button className="s_button">Re-sowing</button>
                                 <button className="s_button">Harvest</button>
